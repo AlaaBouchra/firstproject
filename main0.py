811a78663778bf755c3fd0eb346cb8a79a246619
Alaa Bouchra <admin@MacBook-Air-de-Alaa-Bouchra-Ghembaza.local>
2023-07-18 16:57:17 +0100

    change to the officiale form

--- main0.py
+++ main0.py
@@ -12,7 +12,7 @@ black = (0, 0, 0)
 red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
-#edit test
+
 
 # Set both the width and height of the game window
 dis_width = 600              
